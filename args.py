54801a27abdfc83c74eb75533d66bb2645f6d5f4
huangjiangbo <huangjiangbo@pangqiu.com>
2018-08-10 22:55:42 +0800

    添加对ssh的支持

--- args.py
+++ args.py
@@ -4,6 +4,7 @@ import argparse
 parser = argparse.ArgumentParser(description='ndcu')
 parser.add_argument('-p', default='.', dest='start_path', help='指定开始检索路径, 默认.')
 parser.add_argument('-n', dest='nlarge', default=10, help='指定获取展示大文件数量, 默认10')
+parser.add_argument('-ssh', dest='ssh', default='', help='通过ssh获取远端机器信息')
 
 
 if __name__ == '__main__':
--- extract.py
+++ extract.py
@@ -25,18 +25,18 @@ def extract(file, nlarge=10):
         with gzip.open(file, 'r') as f:
             res = json.load(f)[3]
     except Exception as e:
-        LOG.debug(e)
+        LOG.info(e)
         return
 
     file_list = []
     parent_path = res[0]['name']
     _extract(res[1:], file_list, parent_path)
 
-    ans = heapq.nlargest(nlarge, file_list, key=itemgetter('asize'))
-    LOG.debug('>' * 50)
-    LOG.debug('the resut:\n')
+    ans = heapq.nlargest(nlarge, file_list, key=itemgetter(1))
+    LOG.info('the result (asize: KB):')
+    LOG.info('>' * 50)
     for el in ans:
-        LOG.debug(el)
+        LOG.info(el)
 
 
 def _extract(res, file_list, parent_path):
@@ -50,8 +50,9 @@ def _extract(res, file_list, parent_path):
             _parent_path = os.path.join(parent_path, el[0]['name'])
             _extract(el[1:], file_list, _parent_path)
         elif el.get('asize'):
-            el['name'] = os.path.join(parent_path, el['name'])
-            heapq.heappush(file_list, el)
+            entire_name = os.path.join(parent_path, el['name'])
+            asize = int(el['asize'])
+            heapq.heappush(file_list, (entire_name, asize))
 
 
 def run(args):
@@ -59,20 +60,29 @@ def run(args):
     调用ncdu
     :param args 命令行参数
     """
-    cmd = 'ncdu -1xo- {start_path} | gzip >{export_file}'.format(
-        start_path=args.start_path,
-        export_file=EXPORT_FILE,
-    )
+
+    if args.ssh:
+        cmd = f'{args.ssh} ncdu -1xo- {args.start_path}| gzip >{EXPORT_FILE}'
+    else:
+        cmd = f'ncdu -1xo- {args.start_path} | gzip >{EXPORT_FILE}'
+
     os.system(cmd)
 
 
+def clean():
+    clean_cmd = 'make clean'
+    os.system(clean_cmd)
+
+
 if __name__ == '__main__':
-    init_logger(logging.DEBUG)
+    init_logger(logging.INFO)
 
     start_time = time.time()
     args = parser.parse_args()
     run(args)
-    # 解析中间文件
+
     extract(EXPORT_FILE, int(args.nlarge))
-    LOG.debug('>' * 50)
-    LOG.debug('used time:{}'.format(time.time() - start_time))
+    LOG.info('>' * 50)
+    LOG.info('used time: {}'.format(time.time() - start_time))
+
+    clean()
